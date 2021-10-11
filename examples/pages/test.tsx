f060b61b8c013705848ac52a631fce7f2ac1c313
januwA <ajanuw1995@gmail.com>
2021-10-11 14:36:36 +0800

    :sparkles: 增加对Map和WeakMap数据的支持

--- examples/pages/test.tsx
+++ examples/pages/test.tsx
@@ -1,5 +1,7 @@
 import React, { memo } from "react";
-import { Injectable, RxService, useService, ServiceProxy, noreact, Watch } from "../../src";
+import { Injectable, RxService, useService, ServiceProxy, noreact, Watch, Ignore } from "../../src";
+
+const objKey = {}
 
 @Injectable({ global: false, autoIgnore: true })
 class PS implements ServiceProxy {
@@ -22,13 +24,23 @@ class PS implements ServiceProxy {
   }
 
   @Watch(['this.i', 'this.obj.i'])
-  watch_i(key: string, newVal: number, oldVal: number) {
+  watch(key: string, newVal: number, oldVal: number) {
     console.log(key, newVal, oldVal);
 
   }
 
 
   setObj = new Set([1, 2, 3, { name: 'ajanuw' }])
+
+  mapObj = new Map<any, any>([['a', 1], ['b', 2], ['c', { name: 'ajanuw' }]])
+
+  @Watch(['this.mapObj.a'])
+  watchMapProp(k: any, v: any, ov: any) {
+    console.log(k, v, ov);
+  }
+
+  weakMap = new WeakMap([[objKey, { name: 'ajanuw' }]])
+
 }
 
 export default memo(() => {
@@ -41,11 +53,13 @@ export default memo(() => {
       builder={(c: number) => {
         return (
           <div>
+            <p>{ps.obj.i}</p>
             <p>{ps.i}</p>
             <button onClick={ps.add}>add</button>
             <button onClick={ps.add2}>add(noreact)</button>
             <hr />
-            <h2>{ ps.setObj.size }</h2>
+            <h1>Set 数据测试</h1>
+            <h2>{ps.setObj.size}</h2>
             {Array.from(ps.setObj).map((e, i) => {
               return <p key={i}>{JSON.stringify(e)}
 
@@ -66,6 +80,31 @@ export default memo(() => {
               ps.setObj.delete(2)
             }}>delete</button>
             <hr />
+            <h1>Map 数据测试</h1>
+            <p>{ps.mapObj.get('a')} <button onClick={() => {
+              const ret = ps.mapObj.set('a', '11')
+            }}>set</button>
+
+              <button onClick={() => {
+                ps.mapObj.delete('a')
+              }}>delete</button>
+            </p>
+            <p>{ps.mapObj.get('b')}</p>
+            <p>{JSON.stringify(ps.mapObj.get('c'))}   <button onClick={() => {
+              ps.mapObj.get('c').name = 'suou'
+            }}>change name</button></p>
+
+            <button onClick={() => {
+              ps.mapObj.clear()
+            }}>clear</button>
+            <hr />
+
+            <h1>WeakMap 数据测试</h1>
+            <p>{JSON.stringify(ps.weakMap.get(objKey))} <button onClick={() => {
+              ps.weakMap.get(objKey)!.name = 'suou'
+            }}>change </button>  <button onClick={() => {
+              ps.weakMap.delete(objKey)
+            }}>delete </button></p>
           </div>
         );
       }}
--- src/observable.ts
+++ src/observable.ts
@@ -1,18 +1,23 @@
 import { ServiceManager } from "./ServiceManager";
 import { ServiceIgnore_t } from "./interface";
 
-const IS_PROXY = Symbol('__proxy__')
-const PROXY_SET = Symbol('__proxy_set__')
+const IS_PROXY = Symbol("__proxy__");
+const PROXY_SET = Symbol("__proxy_set__");
 
 function canProxy(obj: any): boolean {
   if (ServiceManager.isService(obj)) return false;
 
   const t = Object.prototype.toString.call(obj);
-  const types = ['[object Object]', '[object Set]']
+  const types = [
+    "[object Object]",
+    "[object Set]",
+    "[object Map]",
+    "[object WeakMap]",
+  ];
   if (Array.isArray(obj) || types.includes(t)) {
     if (Reflect.get(obj, IS_PROXY)) return false;
 
-    return true
+    return true;
   }
   return false;
 }
@@ -27,10 +32,9 @@ function getOwnPropertyDescriptor(
   return getOwnPropertyDescriptor(Object.getPrototypeOf(target), key);
 }
 
-
 export function observable(
   obj: any,
-  watchKey: string = 'this',
+  watchKey: string = "this",
   changed?: (watchKey: string, newValue: any, oldValue: any) => void,
   ignores: ServiceIgnore_t = Object.create(null)
 ) {
@@ -41,10 +45,9 @@ export function observable(
       let v = Reflect.get(t, k);
 
       if (t instanceof Set) {
-        if (k === 'size') return v;
+        if (k === "size") return v;
 
         if (!Reflect.has(t, PROXY_SET)) Reflect.set(t, PROXY_SET, new Map());
-
 
         if (typeof v === "function") {
           const funcProp = v as Function;
@@ -58,7 +61,7 @@ export function observable(
               if (t.has(value)) return t;
 
               const ret = funcProp.apply(t, args);
-              changed?.('', proxy, proxy);
+              changed?.("", proxy, proxy);
               return ret;
             };
           } else if (k === "delete") {
@@ -67,9 +70,9 @@ export function observable(
               const ret = funcProp.apply(t, args);
 
               if (ret) {
-                const _cache: Map<any, any> = Reflect.get(t, PROXY_SET)
+                const _cache: Map<any, any> = Reflect.get(t, PROXY_SET);
                 _cache.delete(args[0]);
-                changed?.('', proxy, proxy);
+                changed?.("", proxy, proxy);
               }
 
               return ret;
@@ -78,45 +81,50 @@ export function observable(
             v = function () {
               const args = Array.from(arguments);
               const ret = funcProp.apply(t, args);
-              const _cache: Map<any, any> = Reflect.get(t, PROXY_SET)
+              const _cache: Map<any, any> = Reflect.get(t, PROXY_SET);
               _cache.clear();
-              changed?.('', proxy, proxy);
+              changed?.("", proxy, proxy);
               return ret;
             };
           } else if (k === "forEach") {
             v = function () {
               const [callbackfn, thisArg] = Array.from(arguments);
 
-              const _cache: Map<any, any> = Reflect.get(t, PROXY_SET)
+              const _cache: Map<any, any> = Reflect.get(t, PROXY_SET);
               return funcProp.apply(t, [
                 function () {
                   const args = Array.from(arguments);
                   const [value] = args;
 
                   if (!_cache.has(value)) {
-                    _cache.set(value, observable(value, '', changed))
+                    _cache.set(value, observable(value, "", changed));
                   }
 
-                  const newval = _cache.get(value)
+                  const newval = _cache.get(value);
                   return callbackfn.apply(thisArg, [newval, newval, proxy]);
                 },
                 thisArg,
               ]);
             };
-          } else if (k === "entries" || k === "keys" || k === "values" || k === Symbol.iterator) {
+          } else if (
+            k === "entries" ||
+            k === "keys" ||
+            k === "values" ||
+            k === Symbol.iterator
+          ) {
             v = function () {
               const args = Array.from(arguments);
               const ret = funcProp.apply(t, args);
 
-              const _cache: Map<any, any> = Reflect.get(t, PROXY_SET)
+              const _cache: Map<any, any> = Reflect.get(t, PROXY_SET);
               for (let it of ret) {
                 if (k === "entries") [it] = it;
                 if (!_cache.has(it)) {
-                  _cache.set(it, observable(it, '', changed))
+                  _cache.set(it, observable(it, "", changed));
                 }
               }
 
-              const values = Array.from(_cache.values()).map(e => [e, e]);
+              const values = Array.from(_cache.values()).map((e) => [e, e]);
 
               let i = 0;
               return {
@@ -128,8 +136,12 @@ export function observable(
                 next() {
                   if (i < values.length) {
                     let value: any = values[i++];
-                    if (k === "keys" || k === "values" || k === Symbol.iterator) {
-                      [value] = value
+                    if (
+                      k === "keys" ||
+                      k === "values" ||
+                      k === Symbol.iterator
+                    ) {
+                      [value] = value;
                     }
                     return { value, done: false };
                   }
@@ -145,7 +157,118 @@ export function observable(
         }
       }
 
+      if (t instanceof Map || t instanceof WeakMap) {
+        if (k === "size") return v;
+        if (typeof v === "function") {
+          const funcProp = v as Function;
+          if (k === "get") {
+            v = function () {
+              const args = Array.from(arguments);
+              let value = funcProp.apply(t, args);
 
+              if (canProxy(value)) {
+                const [key] = args;
+                const _watchKey = `${watchKey}.${key}`;
+                value = observable(value, _watchKey, changed);
+                t.set(key, value);
+              }
+
+              return value;
+            };
+          } else if (k === "set") {
+            v = function () {
+              const args = Array.from(arguments);
+              const [key, newVal] = args;
+              const oldVal = proxy.get(key);
+              if (newVal === oldVal) return t;
+
+              const ret: Map<any, any> = funcProp.apply(t, args);
+              changed?.(`${watchKey}.${key}`, proxy.get(key), oldVal);
+              return ret;
+            };
+          } else if (k === "delete") {
+            v = function () {
+              const args = Array.from(arguments);
+              const [delKey] = args;
+
+              const oldVal = proxy.get(delKey);
+              const success = funcProp.apply(t, args);
+
+              if (success) {
+                changed?.(`${watchKey}.${delKey}`, undefined, oldVal);
+              }
+              return success;
+            };
+          } else if (k === "clear" && t instanceof Map) {
+            v = function () {
+              const args = Array.from(arguments);
+              const kv = Array.from(t.entries());
+              funcProp.apply(t, args);
+
+              for (const [key, oldVal] of kv) {
+                changed?.(`${watchKey}.${key}`, undefined, oldVal);
+              }
+
+              return;
+            };
+          } else if (k === "forEach") {
+            v = function () {
+              const [callbackfn, thisArg] = Array.from(arguments);
+              return funcProp.apply(t, [
+                function () {
+                  const args = Array.from(arguments);
+                  let [value, key] = args;
+
+                  if (canProxy(value)) {
+                    const _watchKey = `${watchKey}.${key}`;
+                    value = observable(value, _watchKey, changed);
+                    t.set(key, value);
+                  }
+
+                  return callbackfn.apply(thisArg, [value, key, proxy]);
+                },
+                thisArg,
+              ]);
+            };
+          } else if (
+            t instanceof Map &&
+            (k === "entries" || k === "values" || k === Symbol.iterator)
+          ) {
+            v = function () {
+              for (let [key, value] of t.entries()) {
+                if (canProxy(value)) {
+                  const _watchKey = `${watchKey}.${key}`;
+                  value = observable(value, _watchKey, changed);
+                  t.set(key, value);
+                }
+              }
+
+              let values: any[] = Array.from(t.entries());
+
+              let i = 0;
+              return {
+                [Symbol.iterator]() {
+                  return {
+                    next: this.next.bind(this),
+                  };
+                },
+                next() {
+                  if (i < values.length) {
+                    let value: any = values[i++];
+                    if (k === "values") value = value[1];
+                    return { value, done: false };
+                  }
+                  return { value: null, done: true };
+                },
+              };
+            };
+          } else {
+            v = v.bind(t);
+          }
+
+          return v;
+        }
+      }
 
       if (k in ignores && ignores[k].get) return v;
 
@@ -158,20 +281,19 @@ export function observable(
 
       if (canProxy(val)) {
         const _watchKey = `${watchKey}.${k}`;
-        const proxyVal = observable(val, _watchKey, changed)
-        Reflect.set(t, k, proxyVal)
+        const proxyVal = observable(val, _watchKey, changed);
+        Reflect.set(t, k, proxyVal);
         val = proxyVal;
       }
       return val;
     },
     set(t: any, k: any, v: any) {
-      const oldValue = Reflect.get(t, k);
-      if (v === oldValue) return true;
+      const oldVal = Reflect.get(t, k);
+      if (v === oldVal) return true;
 
       const isIgnoreTheKey = k in ignores && ignores[k].set;
 
-      if (isIgnoreTheKey)
-        return Reflect.set(t, k, v), true;
+      if (isIgnoreTheKey) return Reflect.set(t, k, v), true;
 
       const _watchKey = `${watchKey}.${k}`;
       const des = getOwnPropertyDescriptor(t, k);
@@ -184,13 +306,13 @@ export function observable(
       else Reflect.set(t, k, v);
 
       if (!isIgnoreTheKey) {
-        changed?.(_watchKey, v, oldValue);
+        changed?.(_watchKey, v, oldVal);
       }
 
       return true;
     },
   });
-  Reflect.set(obj, IS_PROXY, true)
+  Reflect.set(obj, IS_PROXY, true);
 
   return proxy;
 }
